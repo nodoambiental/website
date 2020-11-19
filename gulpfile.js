36482a3d1d840e684e75da5eec19e7b70c6822ff
Ágata Irene Ordano <aordano@protonmail.com>
2020-11-19 14:29:01 -0300

    fix: colors were hard to read in the watch command

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,16 @@ chalk.level = 3;
 
 async function WatchSASS() {
   const controller = new Promise(() => {
-    const command = spawn("sass", ["--source-map","sass:assets/css"]);
+    const command = spawn("sass", ["--source-map","--watch","sass:assets/css"]);
 
     command.stdout.on('data', function (data) {
-      const output = chalk.rgb(144, 238, 144)(`${data.toString()}`);
+      const output = chalk.rgb(178, 218, 101)(`${data.toString()}`);
       const titling = chalk.rgb(170, 152, 169)("stdout | SASS: ");
       console.log(`${titling}${output}`);
     });
     
     command.stderr.on('data', function (data) {
-      const output = chalk.rgb(168, 58, 58)(`${data.toString()}`);
+      const output = chalk.rgb(204, 78, 92)(`${data.toString()}`);
       const titling = chalk.rgb(170, 152, 169)("stderr | SASS: ");
       console.log(`${titling}${output}`);
     });
@@ -23,9 +23,9 @@ async function WatchSASS() {
     command.on('exit', function (code) {
       const titling = chalk.rgb(170, 152, 169)("exit | SASS: ");
       const output = chalk.rgb(240, 240, 240)("Child process exited with code ")
-      let coloredCode = chalk.rgb(144, 238, 144)(code.toString())
+      let coloredCode = `${chalk.rgb(144, 238, 144)(code.toString())} ${chalk.rgb(170, 152, 169)(":D")}`
       if (code !== 0) {
-        coloredCode = chalk.rgb(168, 58, 58)(code.toString())
+        coloredCode = chalk.rgb(204, 78, 92)(code.toString())
       }
       console.log(`${titling}${output}${coloredCode}`);
     });
@@ -40,13 +40,13 @@ async function WatchJekyll() {
     const command = spawn("bundle", ["exec", "jekyll", "serve", "--host", "0.0.0.0"]);
 
     command.stdout.on('data', function (data) {
-      const output = chalk.rgb(144, 238, 144)(`${data.toString()}`);
+      const output = chalk.rgb(178, 218, 101)(`${data.toString()}`);
       const titling = chalk.rgb(170, 152, 169)("stdout | Jekyll: ");
       console.log(`${titling}${output}`);
     });
     
     command.stderr.on('data', function (data) {
-      const output = chalk.rgb(168, 58, 58)(`${data.toString()}`);
+      const output = chalk.rgb(204, 78, 92)(`${data.toString()}`);
       const titling = chalk.rgb(170, 152, 169)("stderr | Jekyll: ");
       console.log(`${titling}${output}`);
     });
@@ -54,9 +54,9 @@ async function WatchJekyll() {
     command.on('exit', function (code) {
       const titling = chalk.rgb(170, 152, 169)("exit | Jekyll: ");
       const output = chalk.rgb(240, 240, 240)("Child process exited with code ")
-      let coloredCode = chalk.rgb(144, 238, 144)(code.toString())
+      let coloredCode = `${chalk.rgb(144, 238, 144)(code.toString())} ${chalk.rgb(170, 152, 169)(":D")}`
       if (code !== 0) {
-        coloredCode = chalk.rgb(168, 58, 58)(code.toString())
+        coloredCode = chalk.rgb(204, 78, 92)(code.toString())
       }
       console.log(`${titling}${output}${coloredCode}`);
     });
