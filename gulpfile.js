0a16ecf6ae4b0f075951c3e7b67b6d72a07994fb
Ágata Irene Ordano <aordano@protonmail.com>
2020-11-19 11:10:21 -0300

    feat: removed Jekyll SASS processor, added Dart and manage it with Gulp

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+const { parallel } = require("gulp");
+const { spawn } = require("child_process");
+const chalk = require("chalk");
+
+chalk.level = 3;
+
+async function WatchSASS() {
+  const controller = new Promise(() => {
+    const command = spawn("sass", ["--source-map","sass:assets/css"]);
+
+    command.stdout.on('data', function (data) {
+      const output = chalk.rgb(144, 238, 144)(`${data.toString()}`);
+      const titling = chalk.rgb(170, 152, 169)("stdout | SASS: ");
+      console.log(`${titling}${output}`);
+    });
+    
+    command.stderr.on('data', function (data) {
+      const output = chalk.rgb(168, 58, 58)(`${data.toString()}`);
+      const titling = chalk.rgb(170, 152, 169)("stderr | SASS: ");
+      console.log(`${titling}${output}`);
+    });
+    
+    command.on('exit', function (code) {
+      const titling = chalk.rgb(170, 152, 169)("exit | SASS: ");
+      const output = chalk.rgb(240, 240, 240)("Child process exited with code ")
+      let coloredCode = chalk.rgb(144, 238, 144)(code.toString())
+      if (code !== 0) {
+        coloredCode = chalk.rgb(168, 58, 58)(code.toString())
+      }
+      console.log(`${titling}${output}${coloredCode}`);
+    });
+
+  })
+
+  return controller
+}
+
+async function WatchJekyll() { 
+  const controller = new Promise(() => {
+    const command = spawn("bundle", ["exec", "jekyll", "serve", "--host", "0.0.0.0"]);
+
+    command.stdout.on('data', function (data) {
+      const output = chalk.rgb(144, 238, 144)(`${data.toString()}`);
+      const titling = chalk.rgb(170, 152, 169)("stdout | Jekyll: ");
+      console.log(`${titling}${output}`);
+    });
+    
+    command.stderr.on('data', function (data) {
+      const output = chalk.rgb(168, 58, 58)(`${data.toString()}`);
+      const titling = chalk.rgb(170, 152, 169)("stderr | Jekyll: ");
+      console.log(`${titling}${output}`);
+    });
+    
+    command.on('exit', function (code) {
+      const titling = chalk.rgb(170, 152, 169)("exit | Jekyll: ");
+      const output = chalk.rgb(240, 240, 240)("Child process exited with code ")
+      let coloredCode = chalk.rgb(144, 238, 144)(code.toString())
+      if (code !== 0) {
+        coloredCode = chalk.rgb(168, 58, 58)(code.toString())
+      }
+      console.log(`${titling}${output}${coloredCode}`);
+    });
+
+  })
+
+  return controller
+}
+
+exports.watch = parallel(WatchSASS, WatchJekyll);
