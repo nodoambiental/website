50cded31736f20077d635320a448542fe7d2e2a7
Agata Ordano <gh@choccymilk.dev>
2023-12-14 13:19:40 -0300

    feat(front): added navbar styles, themes, and script processing and bundling

--- Gemfile
+++ Gemfile
@@ -39,3 +39,5 @@ gem "wdm", "~> 0.1.1", :install_if => Gem.win_platform?
 
 
 gem "webrick", "~> 1.8"
+
+gem "jekyll-webpack", "~> 0.2.7"
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { themeChange } from 'theme-change'
+//import '@fortawesome/fontawesome-free/js/brands.js'
+//import '@fortawesome/fontawesome-free/js/solid.js'
+//import '@fortawesome/fontawesome-free/js/fontawesome.js'
+import '@fortawesome/fontawesome-free/js/all.js'
+themeChange();
